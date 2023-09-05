ee92caa03e01c22f3e96dfc111723d82c85fd224
darhan1168 <darinahanina04@gmail.com>
2023-09-05 14:28:38 +0200

    Delete dots in ValidationManager

--- Core/ValidationManager.cs
+++ Core/ValidationManager.cs
@@ -8,22 +8,22 @@ public static class ValidationManager
 
         if (password.Length < 7)
         {
-            errors.Add("Password must be at least 7 characters long.");
+            errors.Add("Password must be at least 7 characters long");
         }
 
         if (!password.Any(char.IsUpper))
         {
-            errors.Add("Password must contain at least one uppercase letter.");
+            errors.Add("Password must contain at least one uppercase letter");
         }
 
         if (!password.Any(char.IsLower))
         {
-            errors.Add("Password must contain at least one lowercase letter.");
+            errors.Add("Password must contain at least one lowercase letter");
         }
 
         if (!password.Any(char.IsDigit))
         {
-            errors.Add("Password must contain at least one digit.");
+            errors.Add("Password must contain at least one digit");
         }
 
         if (errors.Count == 0)
